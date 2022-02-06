f1151e2fab18631c2641c90dac954c3881bcb160
Jefferson Martins <jef.martins.s@gmail.com>
2022-02-06 15:35:46 -0300

    second commit

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,5 @@
 const app = require("./service");
 const agendamento = require("../Routes/agendamento");
-//const cors = require('cors');
-
-/*******************************/
-/* const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions)); */
-/*******************************/
-
 
 app.listen(8081, () => {
     console.log("Server On in port 8081 at", new Date().toLocaleString());
